d8be5d261fc83b6969c3c3cfad76cd968acf80a8
sauce-chili <saucechilidev@gmail.com>
2024-06-12 16:53:47 +0300

    Refactoring test_toString.cpp

--- tests/test_toString.cpp
+++ tests/test_toString.cpp
@@ -2,86 +2,26 @@
 // Created by sc on 04.06.24.
 //
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
 #include "entities/ExprNode.h"
-#include "entities/Logical/LogicalNode.h"
-#include "entities/Constant/ConstantExprNode.h"
-#include "entities/Arithmetic/ArithmeticNode.h"
 #include "utils/BuilderUtils.h"
 #include "builders/ExprNodeBuilder.h"
 #include "exception/BuildExceptions.h"
-#include <memory>
 
 using namespace std;
 
-TEST(ExprNodeToString, BinaryOperationToString) {
-
-    std::vector<EXPR_NODE_TYPE> binaryOperators = {
-            AND, OR, PLUS, MINUS, MUL, DIV, MOD, XOR, SHLA, SHRA, EQ, NEQ, GREAT, LESS, GREAT_OR_EQ, LESS_OR_EQ
-    };
-
-    bool result = true;
-
-    for (EXPR_NODE_TYPE op: binaryOperators) {
-        string strOp = BuilderUtil::getStrNodeType(op);
-        string inStr = "a b " + strOp;
-
-        list<Exception> exps;
-        ExprNode *root = ExprNodeBuilder::buildExprTree(inStr, exps);
-
-        string expectedStr = "a" + strOp + "b";
-        string actualStr = root->toString();
-
-        delete root;
-
-        if (expectedStr != actualStr) {
-            result = false;
-            break;
-        }
-    }
-
-    EXPECT_TRUE(result);
-}
-
-TEST(ExprNodeToString, UnaryOperationToString) {
-    std::vector<EXPR_NODE_TYPE> unaryOperation = {UNARY_MINUS, NOT};
-
-    bool result = true;
-
-    for (EXPR_NODE_TYPE op: unaryOperation) {
-        string strOp = BuilderUtil::getStrNodeType(op);
-        string inStr = "a " + strOp;
-
-        list<Exception> exps;
-        ExprNode *root = ExprNodeBuilder::buildExprTree(inStr, exps);
-
-        string expectedStr = strOp + "a";
-        string actualStr = root->toString();
-
-        delete root;
-
-        if (expectedStr != actualStr) {
-            result = false;
-            break;
-        }
-    }
-
-    EXPECT_TRUE(result);
-}
-
 using ExceptionList = std::list<Exception>;
 
-struct ExprNodeToStringParams {
+struct ExprNodeToStringTestsParam {
     string testName;
     string buildString;
     string expected;
 };
 
-class ExprNodeToStringTestDDT
-        : public ::testing::TestWithParam<ExprNodeToStringParams> {
+class ExprNodeToStringTest
+        : public ::testing::TestWithParam<ExprNodeToStringTestsParam> {
 };
 
-TEST_P(ExprNodeToStringTestDDT, ExprNodeToString) {
+TEST_P(ExprNodeToStringTest, ParseExprNodeTreeToStringTest) {
     auto params = GetParam();
 
     ExceptionList actualExceptions;
@@ -91,55 +31,129 @@ TEST_P(ExprNodeToStringTestDDT, ExprNodeToString) {
     string actual = tree->toString();
     string expected = params.expected;
 
-    bool result = actual == expected;
+    // ------------------------ удаляем белые разделители ------------------------------------
+    actual.erase(
+            std::remove_if(actual.begin(), actual.end(),
+                           [](unsigned char c) { return std::isspace(c); }),
+            actual.end());
+    expected.erase(
+            std::remove_if(expected.begin(), expected.end(),
+                           [](unsigned char c) { return std::isspace(c); }),
+            expected.end());
+    // ---------------------------------------------------------------------------------------
 
-    EXPECT_TRUE(result);
+    EXPECT_EQ(actual, expected);
 }
 
-vector<ExprNodeToStringParams> testCases = {
-        {
-                "Parse_array_node_to_str",
-                "a i []",
-                "a[i]"
-        },
-        {
-            "Parse_logical_node_with_logical_const_to_str",
-            "a true &&",
-            "a&&true"
-        },
-        {
-            "Parse_arithmetical_node_with_arithmetical_const_to_str",
-            "a 0xFF *",
-            "a*0xFF"
-        },
-        {
-            "Parse_arithmetical_tree_to_str",
-            "c b + a b − +",
-            "c+b+a-b"
-        },
-        {
-            "Parse_tree_with_nodes_different_precedence_lvl_to_str",
-            "c b + a a c - % *",
-            "(c+b)∗(a%(a−c))"
-        },
-        {
-            "Parse_array_tree_to_str",
-            "arr 1 + 0 a b + [] []",
-            "(arr+1)[0][a+b]",
-        },
-        {
-            "Parse_a_complex_tree_to_a_string",
-            "a i [] b * -5 && ! x b a i [] * && ||",
-            "!((a[i]*b)&&-5)||(x && (b*a[i]))"
-        }
+static vector<ExprNodeToStringTestsParam> provideParseSimpleBinaryToStrTestCases() {
 
-};
+    const vector<pair<
+            EXPR_NODE_TYPE, string
+    >
+    > binaryOperators = {
+            {AND,         "AND"},
+            {OR,          "OR"},
+            {PLUS,        "PLUS"},
+            {MINUS,       "MINUS"},
+            {MUL,         "MUL"},
+            {DIV,         "DIV"},
+            {MOD,         "MOD"},
+            {XOR,         "XOR"},
+            {SHLA,        "SHLA"},
+            {SHRA,        "SHRA"},
+            {EQ,          "EQ"},
+            {NEQ,         "NEQ"},
+            {GREAT,       "GREAT"},
+            {LESS,        "LESS"},
+            {GREAT_OR_EQ, "GREAT_OR_EQ"},
+            {LESS_OR_EQ,  "LESS_OR_EQ"}
+    };
+    vector<ExprNodeToStringTestsParam> testCases;
+
+
+    for (const auto &op: binaryOperators) {
+        string strOp = BuilderUtil::getStrRepresentationNodeType(op.first);
+        testCases.push_back(
+                {
+                        "Parse_simple_binary_node_with_root_" + op.second,
+                        "a b " + strOp,
+                        "a" + strOp + "b"
+                }
+        );
+    }
+
+    return testCases;
+}
+
+static vector<ExprNodeToStringTestsParam> provideCommonTestCases() {
+    return {
+            {
+                    "Parse_simple_unary_node_with_root_UNARY_MINUS",
+                    "a -$",
+                    "-a"
+            },
+            {
+                    "Parse_simple_unary_node_with_root_NOT",
+                    "a !",
+                    "!a"},
+            {
+                    "Parse_array_node_to_str",
+                    "a i []",
+                    "a[i]"
+            },
+            {
+                    "Parse_logical_node_with_logical_const_to_str",
+                    "a true &&",
+                    "a && true"
+            },
+            {
+                    "Parse_arithmetical_node_with_arithmetical_const_to_str",
+                    "a 0xFF *",
+                    "a * 0xFF"
+            },
+            {
+                    "Parse_arithmetical_tree_to_str",
+                    "c b + a b − +",
+                    "c + b + a - b"
+            },
+            {
+                    "Parse_tree_with_nodes_different_precedence_lvl_to_str",
+                    "c b + a a c - % *",
+                    "(c + b) ∗ (a % (a − c))"
+            },
+            {
+                    "Parse_array_tree_to_str",
+                    "arr 1 + 0 a b + [] []",
+                    "(arr+1)[0][a+b]",
+            },
+            {
+                    "Parse_a_complex_tree_to_a_string",
+                    "a i [] b * 5 -$ && ! x b a i [] * && ||",
+                    "!((a[i] * b) && -5) || (x && (b * a[i]))"
+            }
+    };
+}
+
+static vector<ExprNodeToStringTestsParam> provideTestCases() {
+
+    auto tests = {
+            provideParseSimpleBinaryToStrTestCases(),
+            provideCommonTestCases(),
+    };
+
+    vector<ExprNodeToStringTestsParam> testCases;
+    for (auto tc: tests) {
+        testCases.insert(testCases.end(), tc.begin(), tc.end());
+    }
+
+    return testCases;
+}
 
 INSTANTIATE_TEST_SUITE_P(
-        ExprNodeToString,
-        ExprNodeToStringTestDDT,
-        ::testing::ValuesIn(testCases),
-        [](const testing::TestParamInfo<ExprNodeToStringParams> &info) {
+        ParseExprNodeTreeToStringTest,
+        ExprNodeToStringTest,
+        ::testing::ValuesIn(provideTestCases()),
+        [](const testing::TestParamInfo<ExprNodeToStringTestsParam> &info) {
             return info.param.testName;
         }
 );
