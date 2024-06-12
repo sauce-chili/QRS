e9f15344fda973e0a0e2b41ae9384923ca18a596
sauce-chili <saucechilidev@gmail.com>
2024-06-12 03:38:19 +0300

    Added test cases covering all possible combinations of operand values of logical operations

--- tests/test_calculate.cpp
+++ tests/test_calculate.cpp
@@ -2,16 +2,9 @@
 // Created by sc on 04.06.24.
 //
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
 #include "entities/ExprNode.h"
-#include "entities/Logical/LogicalNode.h"
-#include "entities/Constant/ConstantExprNode.h"
-#include "entities/Arithmetic/ArithmeticNode.h"
-#include "utils/BuilderUtils.h"
 #include "builders/ExprNodeBuilder.h"
 #include "exception/BuildExceptions.h"
-#include <bitset>
-
 
 using namespace std;
 using ExceptionList = std::list<Exception>;
@@ -34,58 +27,124 @@ TEST_P(CalculateTestDDT, CalculateTest) {
     string inStr = params.buildString;
     ExprNode *tree = ExprNodeBuilder::buildExprTree(inStr, actualExceptions);
 
-    vector<ExprNode*> parameters;
+    vector<ExprNode *> parameters;
     tree->getParameters(parameters);
     bool actualResult = tree->calculate(params.args);
     bool expectedResult = params.expected;
 
-    EXPECT_EQ(actualResult,expectedResult);
+    EXPECT_EQ(actualResult, expectedResult);
 }
 
-vector<CalculateTestParams> testCases = {
+const vector<CalculateTestParams> commonTestCases = {
         {
-            "AND_with_constants",
-            "a true 8.1 && &&",
-            0b1,
-            true
+                "AND_with_parameters_value_00",
+                "a b &&",
+                0b00,
+                false,
         },
         {
-            "OR_with_constants",
-            "a false 0 || ||",
-            0b0,
-            false
+                "AND_with_parameters_value_01",
+                "a b &&",
+                0b10,
+                false,
         },
         {
-            "NOT_with_constant",
-            "a !",
-            0b0,
-            true
+                "AND_with_parameters_value_10",
+                "a b &&",
+                0b01,
+                false,
         },
         {
-            "The_whole_expression_consists_of_constants",
-            "true false || 8.2 0b010 && ||",
-            0b0,
-            false,
+                "AND_with_parameters_value_11",
+                "a b &&",
+                0b11,
+                false,
         },
         {
-                "The_whole_expression_consists_of_simple_parameters",
+                "OR_with_parameters_value_00",
+                "a b ||",
+                0b00,
+                false,
+        },
+        {
+                "OR_with_parameters_value_01",
+                "a b ||",
+                0b10,
+                true,
+        },
+        {
+                "OR_with_parameters_value_10",
+                "a b ||",
+                0b01,
+                true,
+        },
+        {
+                "OR_with_parameters_value_11",
+                "a b ||",
+                0b11,
+                true,
+        },
+        {
+                "NOT_with_parameters_value_0",
+                "a !",
+                0b0,
+                true
+        },
+        {
+                "NOT_with_parameters_value_1",
+                "a !",
+                0b1,
+                false
+        },
+        {
+                "AND_with_constants",
+                "a true 8.1 && &&",
+                0b1,
+                true
+        },
+        {
+                "OR_with_constants",
+                "a false 0 || ||",
+                0b0,
+                false
+        },
+        {
+                "NOT_with_constant",
+                "false !",
+                0b0,
+                true
+        },
+        {
+                "The_whole_expression_consists_of_constants",
+                "true false || 8.2 0b010 && ||",
+                0b0,
+                false,
+        },
+        {
+                "The_whole_expression_consists_of_simple_parameters_1",
                 "c a b && ||",
                 0b110,
                 true
         },
         {
-            // !((a[i]*b)&&(-0xFA+5)) || x && (b*a[i]) && (c+k)
-            "Сalculation_of_complex_expression",
-            "a i [] b * 0xFA -$ 5 + && ! x b a i [] * c k + && && ||",
-            0b110,
-            true
+                "The_whole_expression_consists_of_simple_parameters_2",
+                "c a b && || !",
+                0b110,
+                false
+        },
+        {
+                // !((a[i]*b)&&(-0xFA+5)) || x && (b*a[i]) && (c+k)
+                "Calculation_of_complex_expression",
+                "a i [] b * 0xFA -$ 5 + && ! x b a i [] * c k + && && ||",
+                0b110,
+                true
         }
 };
 
 INSTANTIATE_TEST_SUITE_P(
         CalculateTest,
         CalculateTestDDT,
-        ::testing::ValuesIn(testCases),
+        ::testing::ValuesIn(commonTestCases),
         [](const testing::TestParamInfo<CalculateTestParams> &info) {
             return info.param.testName;
         }
