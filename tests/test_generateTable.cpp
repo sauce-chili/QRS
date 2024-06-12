53acff41ea93bbd9608d8f1b0dcc5ccfe6d6f5ec
sauce-chili <saucechilidev@gmail.com>
2024-06-13 01:51:36 +0300

    Refactoring with using utils function and add 1 test case

--- tests/test_generateTable.cpp
+++ tests/test_generateTable.cpp
@@ -2,25 +2,20 @@
 // Created by sc on 04.06.24.
 //
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
 #include "entities/ExprNode.h"
-#include "entities/Logical/LogicalNode.h"
-#include "entities/Constant/ConstantExprNode.h"
-#include "entities/Arithmetic/ArithmeticNode.h"
-#include "utils/BuilderUtils.h"
 #include "builders/ExprNodeBuilder.h"
 #include "exception/BuildExceptions.h"
 #include "truthtablegererator/Generator.h"
-#include <bitset>
-
+#include "TestUtils.h"
 
 using namespace std;
-using ExceptionList = std::list<Exception>;
 
 struct GenerateTableTestParams {
     string testName;
     string buildString;
-    string expectedTable
+    vector<string> expectedHeader;
+    vector<vector<string>> expectedRowValues;
+    bool is_throwable = false;
 };
 
 class GenerateTableTestDDT
@@ -30,110 +25,115 @@ class GenerateTableTestDDT
 TEST_P(GenerateTableTestDDT, GenerateTableTest) {
     auto params = GetParam();
 
-    string actualResult = TruthTableGenerator::createRow(
-            params.args,
-            params.countArgs,
-            params.result
-    );
-    string expectedResult = params.expectedRow;
+    ExceptionList exps;
+    ExprNode *tree = ExprNodeBuilder::buildExprTree(params.buildString, exps);
 
-    EXPECT_EQ(actualResult, expectedResult);
+    string actualTable;
+
+    if (params.is_throwable) {
+        EXPECT_THROW(
+                TruthTableGenerator::generateTable(tree),
+                ExceedingLimitParametersException
+        );
+    } else {
+        actualTable = TruthTableGenerator::generateTable(tree);
+    }
+
+    vector<string> expectedHeader = params.expectedHeader;
+    vector<vector<string>> expectedRowsValues = params.expectedRowValues;
+
+    vector<string> actualHeader = extractHeaders(actualTable);
+    vector<vector<string>> actualRowsValues = extractValues(actualTable);
+
+    EXPECT_EQ(expectedHeader, actualHeader);
+    EXPECT_EQ(expectedRowsValues, actualRowsValues);
 }
 
-vector<GenerateTableTestParams> testCases = {
+const vector<GenerateTableTestParams> commonTestCases = {
         {
                 "Generate_table_with_one_parameter",
                 "true a -$ 0xFA + &&",
-                R"SONG(
-<table>
-<tr>
-    <td>a+(-0xFA)</td><td>F</td>
-</tr>
-<tr>
-    <td>0</td><td>0</td>
-</tr>
-<tr>
-    <td>1</td><td>1</td>
-</tr>
-</table>
-)SONG"
+                {"a+(-0xFA)", "F"},
+                {
+                        {"0", "0"},
+                        {"1", "1"}
+                }
         },
         {
                 "Generate_table_with_several_parameters",
                 "a i [] b * 0xFA -$ 5 + && ! x c k + && ||",
-                R"SONG(
-<table>
-<tr>
-    <td>a[i]*b</td><td>x</td><td>c+k</td><td>F</td>
-</tr>
-<tr>
-    <td>0</td><td>0</td><td>0</td><td>1</td>
-</tr>
-<tr>
-    <td>0</td><td>0</td><td>1</td><td>1</td>
-</tr>
-<tr>
-    <td>0</td><td>1</td><td>0</td><td>1</td>
-</tr>
-<tr>
-    <td>0</td><td>1</td><td>1</td><td>1</td>
-</tr>
-<tr>
-    <td>1</td><td>0</td><td>0</td><td>0</td>
-</tr>
-<tr>
-    <td>1</td><td>0</td><td>1</td><td>0</td>
-</tr>
-<tr>
-    <td>1</td><td>1</td><td>0</td><td>0</td>
-</tr>
-<tr>
-    <td>1</td><td>1</td><td>1</td><td>1</td>
-</tr>
-</table>
-)SONG"
+                {"a[i]*b",    "x",          "c+k", "F"},
+                {
+                        // <td>0</td><td>0</td><td>0</td><td>1</td>
+                        {"0", "0", "0", "0"},
+                        // <td>0</td><td>0</td><td>1</td><td>1</td>
+                        {"0", "0", "1", "1"},
+                        // <td>0</td><td>1</td><td>0</td><td>1</td>
+                        {"0", "1", "0", "1"},
+                        // <td>0</td><td>1</td><td>1</td><td>1</td>
+                        {"0", "1", "1", "1"},
+                        // <td>1</td><td>0</td><td>0</td><td>0</td>
+                        {"1", "0", "0", "0"},
+                        // <td>1</td><td>0</td><td>1</td><td>0</td>
+                        {"1", "0", "1", "0"},
+                        // <td>1</td><td>1</td><td>0</td><td>0</td>
+                        {"1", "1", "0", "0"},
+                        // <td>1</td><td>1</td><td>1</td><td>1</td>
+                        {"1", "1", "1", "1"}
+                },
         },
         {
-            "Generating_table_with_several_parameters_including_commutative_parameters"
-            "a i [] b * 0xFA -$ 5 + && ! x b a i [] * c k + && && ||",
-                R"SONG(
-<table>
-<tr>
-    <td>a[i]*b</td><td>x</td><td>c+k</td><td>F</td>
-</tr>
-<tr>
-    <td>0</td><td>0</td><td>0</td><td>1</td>
-</tr>
-<tr>
-    <td>0</td><td>0</td><td>1</td><td>1</td>
-</tr>
-<tr>
-    <td>0</td><td>1</td><td>0</td><td>1</td>
-</tr>
-<tr>
-    <td>0</td><td>1</td><td>1</td><td>1</td>
-</tr>
-<tr>
-    <td>1</td><td>0</td><td>0</td><td>0</td>
-</tr>
-<tr>
-    <td>1</td><td>0</td><td>1</td><td>0</td>
-</tr>
-<tr>
-    <td>1</td><td>1</td><td>0</td><td>0</td>
-</tr>
-<tr>
-    <td>1</td><td>1</td><td>1</td><td>1</td>
-</tr>
-</table>
-)SONG"
+                "Generating_table_with_several_parameters_including_commutative_parameters",
+                "a i [] b * 0xFA -$ 5 + && ! x b a i [] * c k + && && ||",
+                {"a[i]*b",    "x",          "c+k", "F"},
+                {
+                        // <td>0</td><td>0</td><td>0</td><td>1</td>
+                        {"0", "0", "0", "1"},
+                        // <td>0</td><td>0</td><td>1</td><td>1</td>
+                        {"0", "0", "1", "1"},
+                        // <td>0</td><td>1</td><td>0</td><td>1</td>
+                        {"0", "1", "0", "1"},
+                        // <td>0</td><td>1</td><td>1</td><td>1</td>
+                        {"0", "1", "1", "1"},
+                        // <td>1</td><td>0</td><td>0</td><td>0</td>
+                        {"1", "0", "0", "0"},
+                        // <td>1</td><td>0</td><td>1</td><td>0</td>
+                        {"1", "0", "1", "0"},
+                        // <td>1</td><td>1</td><td>0</td><td>0</td>
+                        {"1", "1", "0", "0"},
+                        // <td>1</td><td>1</td><td>1</td><td>1</td>
+                        {"1", "1", "1", "1"}
+                }
+        },
+        // ! x && (-(a[i] && b) || !a[i]) || a[i] && (-(a[i] && b) || x)
+        {
+                "One_parameter_is_nested_within_another",
+                "x ! a i [] b && -$ a i [] ! || && a i [] a i [] b && -$ x || && ||",
+                {"x",         "-(a[i]||b)", "a[i]"},
+                {
+                        {"0", "0", "0", "1"},
+                        {"0", "0", "1", "0"},
+                        {"0", "1", "0", "0"},
+                        {"0", "1", "1", "1"},
+                        {"1", "0", "0", "1"},
+                        {"1", "0", "1", "1"},
+                        {"1", "1", "0", "0"},
+                        {"1", "1", "1", "1"}
+                }
+        },
+        {
+                "Parameter_limit_exceeded",
+                "p1 p2 p3 p4 p5 p6 p7 p8 p9 p10 p11 || || || || || || || || || || ||",
+                {},
+                {},
+                true
         }
 };
 
 INSTANTIATE_TEST_SUITE_P(
         GenerateTableTest,
         GenerateTableTestDDT,
-        ::testing::ValuesIn(testCases),
+        ::testing::ValuesIn(commonTestCases),
         [](const testing::TestParamInfo<GenerateTableTestParams> &info) {
             return info.param.testName;
         }
