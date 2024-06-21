f27ea5ce766304c69a52ca3bcef9e41bb272b9e6
sauce-chili <saucechilidev@gmail.com>
2024-06-21 16:03:41 +0300

    Implemented truth table generator

---
+++ src/truthtablegererator/Generator.cpp
@@ -0,0 +1,80 @@
+//
+// Created by sc on 22.04.24.
+//
+
+#include "Generator.h"
+#include "utils/BuilderUtils.h"
+#include "utils/Utils.h"
+#include "exception/BuildExceptions.h"
+#include <string>
+#include <cmath>
+
+const std::string TruthTableGenerator::CELL_TAGS[2] = {"<td>", "</td>"};
+const std::string TruthTableGenerator::ROW_TAGS[2] = {"<tr>", "</tr>"};
+const std::string TruthTableGenerator::TABLE_TAGS[2] = {"<table>", "</table>"};
+
+std::string TruthTableGenerator::generateTable(ExprNode *root) {
+
+    std::vector<ExprNode *> params;
+    root->getParameters(params);
+
+    int countParameters = params.size();
+
+    if (countParameters > MAX_COLUMN) {
+        throw ExceedingLimitParametersException();
+    }
+
+    std::string table;
+    table += createHeader(params);
+    table += createRowsOfValues(root, countParameters);
+    table = BuilderUtil::wrapIn(TABLE_TAGS, table);
+
+    return table;
+}
+
+std::string TruthTableGenerator::createHeader(std::vector<ExprNode *> &params) {
+    std::string header;
+
+    for (auto p: params) {
+        std::string val = p->toString();
+        header += createCell(val);
+    }
+    header += createCell("F");
+
+    header = BuilderUtil::wrapIn(ROW_TAGS, header);
+
+    return header;
+}
+
+std::string TruthTableGenerator::createCell(const std::string &val) {
+    std::string cell = BuilderUtil::wrapIn(CELL_TAGS, val);
+    return cell;
+}
+
+std::string TruthTableGenerator::createRowsOfValues(ExprNode *root, int countOfParams) {
+    std::string rowsOfValues;
+    int countOfRow = pow(2, countOfParams);
+    for (ushort p = 0; p < countOfRow; p++) {
+        bool result = root->calculate(p);
+        rowsOfValues += createRow(p, countOfParams, result);
+    }
+
+    return rowsOfValues;
+}
+
+std::string TruthTableGenerator::createRow(unsigned short args, int countColumns, bool result) {
+
+    std::string row;
+    for (ushort bitIdx = 0; bitIdx < countColumns; bitIdx++) {
+
+        int bit = extractBitOfNumber(args, bitIdx + 1);
+
+        row = createCell(std::to_string(bit)) + row;
+    }
+
+    row += createCell(std::to_string(result));
+
+    row = BuilderUtil::wrapIn(ROW_TAGS, row);
+
+    return row;
+}
---
+++ src/truthtablegererator/Generator.h
@@ -0,0 +1,44 @@
+//
+// Created by sc on 22.04.24.
+//
+
+#ifndef MODULWORD_Q_RS_GENERATOR_H
+#define MODULWORD_Q_RS_GENERATOR_H
+
+
+#include <list>
+#include "../entities/ExprNode.h"
+
+class TruthTableGenerator {
+private:
+    static const std::string CELL_TAGS[2];
+    static const std::string ROW_TAGS[2];
+    static const std::string TABLE_TAGS[2];
+
+    static std::string createCell(const std::string &val);
+
+    static std::string createRowsOfValues(ExprNode *root, int countOfParams);
+
+    static std::string createHeader(std::vector<ExprNode *> &params);
+
+public:
+    static const uint MAX_COLUMN = 10;
+
+    /*!
+     * @brief Функция создаёт html-код строки значений параметров
+     * @param[in] args - значения принимаемые параметрами
+     * @param[in] countColumns - кол-во параметров
+     * @param[in] result - значение принимаемое логическим выражением
+     * */
+    static std::string createRow(unsigned short args, int countColumns, bool result);
+
+    /*!
+     * @brief Функция создает html-код таблицы истинности для переданного дерева логического выражения
+     * @param[in] root - корень дерева логического выражения
+     * @return html-код таблицы истинности
+     * */
+    static std::string generateTable(ExprNode *root);
+};
+
+
+#endif //MODULWORD_Q_RS_GENERATOR_H
