4721507d7e1dbabad70f53518a3c1c4c94d58c6c
sauce-chili <saucechilidev@gmail.com>
2024-06-22 02:57:51 +0300

    uncomment

--- src/main.cpp
+++ src/main.cpp
@@ -8,55 +8,55 @@
 using namespace std;
 
 
-//int main(int argc, char *argv[]) {
-//
-//    if (argc != 3) {
-//        std::cerr << "Используйте: " << argv[0]
-//                  << " <путь до файла с деревом разбора выражения> <путь до выходного файла>" << std::endl;
-//        return 1;
-//    }
-//
-//    string pathToInputFile = argv[1];
-//    string pathToOutputFile = argv[2];
-//
-//    ifstream inFile(pathToInputFile);
-//    if (!inFile.is_open()) {
-//        std::cerr << "Не удалось открыть файл: " << pathToInputFile << std::endl;
-//        return 1;
-//    }
-//
-//    string postfixTree;
-//    getline(inFile, postfixTree);
-//    inFile.close();
-//
-//    list<Exception> exps;
-//    ExprNode *tree = ExprNodeBuilder::buildExprTree(postfixTree, exps);
-//
-//    if (not exps.empty()) {
-//        for (const auto &e: exps) {
-//            std::cerr << e.what() << endl;
-//        }
-//        return 1;
-//    }
-//
-//    string htmlTable;
-//
-//    try {
-//        htmlTable = TruthTableGenerator::generateTable(tree);
-//    } catch (Exception &e) {
-//        std::cerr << e.what() << endl;
-//        return 1;
-//    }
-//
-//    // Открываем выходной файл
-//    std::ofstream outFile(pathToOutputFile);
-//    if (!outFile.is_open()) {
-//        std::cerr << "Не удалось получить доступ к выходному файлу: " << pathToOutputFile << std::endl;
-//        return 1;
-//    }
-//
-//    outFile << htmlTable;
-//    outFile.close();
-//
-//    return 0;
-//}
+int main(int argc, char *argv[]) {
+
+    if (argc != 3) {
+        std::cerr << "Используйте: " << argv[0]
+                  << " <путь до файла с деревом разбора выражения> <путь до выходного файла>" << std::endl;
+        return 1;
+    }
+
+    string pathToInputFile = argv[1];
+    string pathToOutputFile = argv[2];
+
+    ifstream inFile(pathToInputFile);
+    if (!inFile.is_open()) {
+        std::cerr << "Не удалось открыть файл: " << pathToInputFile << std::endl;
+        return 1;
+    }
+
+    string postfixTree;
+    getline(inFile, postfixTree);
+    inFile.close();
+
+    list<Exception> exps;
+    ExprNode *tree = ExprNodeBuilder::buildExprTree(postfixTree, exps);
+
+    if (not exps.empty()) {
+        for (const auto &e: exps) {
+            std::cerr << e.what() << endl;
+        }
+        return 1;
+    }
+
+    string htmlTable;
+
+    try {
+        htmlTable = TruthTableGenerator::generateTable(tree);
+    } catch (Exception &e) {
+        std::cerr << e.what() << endl;
+        return 1;
+    }
+
+    // Открываем выходной файл
+    std::ofstream outFile(pathToOutputFile);
+    if (!outFile.is_open()) {
+        std::cerr << "Не удалось получить доступ к выходному файлу: " << pathToOutputFile << std::endl;
+        return 1;
+    }
+
+    outFile << htmlTable;
+    outFile.close();
+
+    return 0;
+}
