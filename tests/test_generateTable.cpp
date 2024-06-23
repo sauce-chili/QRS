c3439a30bcad126f3b3aaffe495183d5756df3a6
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-06-23 07:51:47 +0300

    Remove extra operator in test case

--- tests/test_generateTable.cpp
+++ tests/test_generateTable.cpp
@@ -142,7 +142,7 @@ const vector<GenerateTableTestParams> commonTestCases = {
         // TEST 6
         {
                 "Parameter_limit_exceeded",
-                "p1 p2 p3 p4 p5 p6 p7 p8 p9 p10 p11 || || || || || || || || || || ||",
+                "p1 p2 p3 p4 p5 p6 p7 p8 p9 p10 p11 || || || || || || || || || ||",
                 {},
                 {},
                 true
